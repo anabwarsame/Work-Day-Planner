d8c68f1e8f047aee7c45c2bd6d3173feddcee661
anabwarsame <anab_1994@hotmail.co.uk>
2022-02-18 19:50:52 +0000

    wip

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,48 @@ $(document).ready(function () {
 
   clock.innerHTML = moment().format("dddd, Do MMMM  YYYY, h:mm:ss a");
 
-  const saveBtn = document.getElementById("saveBtn");
-  // when you click btn text from input should be saved
+  function currentTime() {
+    const current = moment().hours();
+    const time = $(".timeblock");
 
-  const textBox = document.getElementById("text-box").textContent;
-  console.log(textBox);
+    time.each(function () {
+      const hour = parseInt($(this).attr("id"));
 
-  const time = document.getElementById("timeblock").textContent;
-  console.log(time);
-  // button click isnt being picked up currently
+      if (hour === current) {
+        $(this).children(".textbox").attr("class", "present textbox");
+      } else if (current > hour) {
+        $(this).children(".textbox").attr("class", "past textbox");
+      } else {
+        $(this).children(".textbox").attr("class", "future textbox");
+      }
+    });
+  }
 
-  saveBtn.addEventListener("click", () => {
-    // currently only listening out for 1pm button click
+  currentTime();
+
+  const button = $(".saveBtn");
+
+  button.on("click", function () {
+    const time = $(this).parent().attr("id");
+    const textBox = $(this).siblings(".textbox").val();
 
     localStorage.setItem(time, textBox);
-    console.log("hello");
   });
 });
 
-// setInterval(() => {}, 1000);
-
-// // when you click save button
 // const saveBtn = document.getElementById("saveBtn");
 // // when you click btn text from input should be saved
+
 // const textBox = document.getElementById("textbox").textContent;
-// saveBtn.addEventListener("click", (event) => {
-//   localStorage.setItem("1PM", textBox);
+// console.log(textBox);
+
+// const time = document.getElementById("timeblock").textContent;
+// console.log(time);
+// // button click isnt being picked up currently
+
+// saveBtn.addEventListener("click", function () {
+//   // currently only listening out for 1pm button click
+
+//   localStorage.setItem(time, textBox);
 //   console.log("hello");
 // });
-// local storage
-
-// const getFromLocalStorage = function (key, defaultValue) {
-//   const localStorageData = JSON.parse(localStorage.getItem(key));
-
-//   if (!localStorageData) {
-//     return defaultValue;
-//   } else {
-//     return localStorageData;
-//   }
-// };
